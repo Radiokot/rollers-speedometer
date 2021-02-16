d9b6fa99627ffa353c01a645201cceed02fc49af
Oleg Koretsky <test.radiokot@gmail.com>
2021-02-17 00:48:33 +0200

    Implement more accurate approach of rotations counting and speed calculation

--- Speedometer.ino
+++ Speedometer.ino
@@ -1,5 +1,5 @@
 #define HALL_PIN          11
-#define ROLLER_CIRCLE_MM  264 
+#define ROLLER_CIRCLE_MM  264
 
 void setup()
 {
@@ -7,43 +7,65 @@ void setup()
 }
 
 unsigned char measuredState = 0;
-bool skipNext = true;
+bool skipNextStateChange = true;
 unsigned int rotations = 0;
-unsigned long calculationTimeMs = 0;
+unsigned int rotationsTotalTimeMs = 0;
+unsigned long lastRotationTimeMs = 0;
+unsigned long lastReportTimeMs = 0;
 
 void loop()
 {
-  checkRotation();
-
   unsigned long nowMs = millis();
-  unsigned long timeDeltha = nowMs - calculationTimeMs;
+  
+  checkRotation(nowMs);
 
-  if (timeDeltha > 1000) {
-    calculationTimeMs = nowMs;
-    calculateAndReportSpeed(timeDeltha);
+  // Report the data if it's significant 
+  // or if nothing has been reported for a long time.
+  if (rotationsTotalTimeMs > 1000 || nowMs - lastReportTimeMs > 2000) {
+    reportData(nowMs);
+
+    // If we had no rotations for a long time, 
+    // then last rotation time is no more relevant.
+    if (rotations == 0) {
+      lastRotationTimeMs = 0;
+    }
+    
+    rotations = 0;
+    rotationsTotalTimeMs = 0;
   }
 }
 
-void checkRotation() {
+void checkRotation(unsigned long nowMs) {
   uint8_t state = digitalRead(HALL_PIN);
-  
+
   if (state != measuredState) {
     measuredState = state;
-    if (!skipNext) {
-      rotations++;
-      skipNext = true;
+    if (!skipNextStateChange) {
+      // We can only count the rotation 
+      // if we know it's duration.
+      if (lastRotationTimeMs != 0) {
+        rotations++;
+        rotationsTotalTimeMs += nowMs - lastRotationTimeMs;
+      }
+      
+      lastRotationTimeMs = nowMs;
+      
+      // Next state change will be caused by the opposite 
+      // magnet pole so we have to skip it.
+      skipNextStateChange = true;
     } else {
-      skipNext = false;
+      skipNextStateChange = false;
     }
   }
 }
 
-void calculateAndReportSpeed(unsigned long periodMs) {
+void reportData(unsigned long nowMs) {
   unsigned int passedDistanceMm = rotations * ROLLER_CIRCLE_MM;
-  unsigned int speedMph = passedDistanceMm * 3600.0 / periodMs;
-  rotations = 0;
-  Serial.print("S ");
+  unsigned int speedMph = (rotationsTotalTimeMs != 0)
+                          ? passedDistanceMm * 3600.0 / rotationsTotalTimeMs
+                          : 0;
+  lastReportTimeMs = nowMs;
   Serial.print(speedMph);
-  Serial.print(", D ");
+  Serial.print(" ");
   Serial.println(passedDistanceMm);
 }
